b55ea7f5583d0d8660fd9d144ac3f8ad71031452
Azrul Haifan <ahaifan@gmail.com>
2024-10-16 08:29:44 +0700

    Add ability to use azurite connection string from .env

--- api/script/storage/azure-storage.ts
+++ api/script/storage/azure-storage.ts
@@ -856,10 +856,10 @@ export class AzureStorage implements storage.Storage {
     let blobServiceClient: BlobServiceClient;
 
     if (process.env.EMULATED) {
-      const devConnectionString = "UseDevelopmentStorage=true";
+      const devConnectionString = process.env.AZURITE_CONNECTION_STRING;
 
-      tableServiceClient = TableServiceClient.fromConnectionString(devConnectionString);
-      tableClient = TableClient.fromConnectionString(devConnectionString, AzureStorage.TABLE_NAME);
+      tableServiceClient = TableServiceClient.fromConnectionString(devConnectionString, {allowInsecureConnection : true});
+      tableClient = TableClient.fromConnectionString(devConnectionString, AzureStorage.TABLE_NAME, {allowInsecureConnection : true});
       blobServiceClient = BlobServiceClient.fromConnectionString(devConnectionString);
     } else {
       if ((!accountName && !process.env.AZURE_STORAGE_ACCOUNT) || (!accountKey && !process.env.AZURE_STORAGE_ACCESS_KEY)) {
